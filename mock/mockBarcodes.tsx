f3443395c2340f3d0232c5d0e8e79d55a1aa5a3b
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-10-30 16:17:10 +0300

    mock qr codes for development

---
+++ mock/mockBarcodes.tsx
@@ -0,0 +1,8 @@
+export default {
+  qr:
+    "https://www.cs.columbia.edu/~hgs/research/projects/iPhone-barcode/google.png",
+  datamatrix:
+    "https://www.cs.columbia.edu/~hgs/research/projects/iPhone-barcode/googledmtx.png",
+  test:
+    "http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data=SMSTO%3A905542221144%3Ahi+there&qzone=1&margin=0&size=400x400&ecc=L",
+};
