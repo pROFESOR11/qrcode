e702db2a89adc970f92847797fd8e2410e0a0363
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-10 23:38:25 +0300

    change statusbar theme to light-content

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { BottomTabNavigator } from "./navigation/BottomNavigator";
+import { BottomTabNavigator } from "./navigation/tab/BottomNavigator";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" />
       <BottomTabNavigator />
     </NavigationContainer>
   );
