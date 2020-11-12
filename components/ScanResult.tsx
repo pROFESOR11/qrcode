15a62d45eed63a8301a9c25cb9c3a98aa79aea70
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-12 10:01:00 +0300

    scanResult initial

--- components/ScanResult.tsx
+++ components/ScanResult.tsx
@@ -1,17 +1,22 @@
-import { BarCodeScannerResult } from "expo-barcode-scanner";
+import { BarCodeEvent, BarCodeScannerResult } from "expo-barcode-scanner";
 import React from "react";
-import { Card, Text } from "react-native-elements";
+import { View } from "react-native";
+import { Card, Text, Button } from "react-native-elements";
+import { detectBarcodeTypes } from "../utils/detectBarcodeType";
+import { Btn } from "./Btn";
 
-interface ScanResultProps {
-  scanResult: BarCodeScannerResult;
-}
-
-export const ScanResult = ({ scanResult }: ScanResultProps) => {
+export const ScanResult = ({ scanResult }: { scanResult?: BarCodeEvent }) => {
   return (
     <Card>
       <Card.Title>Scan Results</Card.Title>
       <Card.Divider />
-      <Text>{scanResult.data ?? "Something went wrong!"}</Text>
+      <Text>{scanResult?.data ?? "Something went wrong!"}</Text>
+      <Card.Divider />
+      <View style={{ flexDirection: "row" }}>
+        <Btn title="SMS" />
+        <Btn title="Call" />
+        <Btn title="Mail" />
+      </View>
     </Card>
   );
 };
