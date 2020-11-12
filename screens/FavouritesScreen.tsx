2135e63ae5e6c5d607cb8130b4c8df7992f69b8e
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-12 10:02:44 +0300

    favourites initial

---
+++ screens/FavouritesScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+interface FavouritesScreenProps {}
+
+export const FavouritesScreen: React.FC<FavouritesScreenProps> = ({}) => {
+  return (
+    <View>
+      <Text>Favourites</Text>
+    </View>
+  );
+};
