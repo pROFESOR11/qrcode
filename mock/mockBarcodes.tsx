8244994822a2b7cbb9baa1d866ad580efd6ba362
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-16 01:43:30 +0300

    add mockBarcode urls for each type

--- mock/mockBarcodes.tsx
+++ mock/mockBarcodes.tsx
@@ -1,6 +1,21 @@
 export default {
-  qr:
-    "https://www.cs.columbia.edu/~hgs/research/projects/iPhone-barcode/google.png",
+  qr: {
+    calendar:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=BEGIN%3AVEVENT%0D%0ASUMMARY%3AI+am+title!%0D%0ADTSTART%3A20201112T014800Z%0D%0ADTEND%3A20201118T184500Z%0D%0ALOCATION%3AIstanbul+Location%0D%0ADESCRIPTION%3AHi%2C+I+am+a+description%0D%0AEND%3AVEVENT%0D%0A",
+    vCard:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3Aname+name2%0AORG%3Acompany+company2%0ATITLE%3Atitle+title2%0ATEL%3A23123012301230%0AURL%3Ahttp%3A%2F%2Fwebstie.ur.lcom%0AEMAIL%3Aasdasdo%40coasd.com%0AADR%3Aadres1-1+adres1-2+adres2-1+adres2-2%0ANOTE%3Amemo1+memo2%0AEND%3AVCARD",
+    email:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=mailto%3Aasdas%40asda.com",
+    geo:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=geo%3A25.558%2C-18.26569%3Fq%3Di+am+query+here",
+    phone:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=tel%3A123123321",
+    sms:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=smsto%3A5548125425%3Ahi%2Cwhats+up%3F",
+    url:
+      "https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=http%3A%2F%2Fasdasd.com",
+  },
+
   datamatrix:
     "https://www.cs.columbia.edu/~hgs/research/projects/iPhone-barcode/googledmtx.png",
   test:
