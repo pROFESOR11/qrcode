379bc8516465da2b447efe8e912ee1a87ffcc2ea
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-16 19:06:17 +0300

    read data from AsyncStorage in ScannerResult screen this will provide consistency with history and favourites screens

--- screens/ScanResultScreen.tsx
+++ screens/ScanResultScreen.tsx
@@ -1,17 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRoute } from "@react-navigation/native";
 import { BarCodeEvent, BarCodeScannerResult } from "expo-barcode-scanner";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { HistoryItem } from "../components/HistoryItem";
 import { ScanResult } from "../components/ScanResult";
 import { ScanResultScreenRouteProp } from "../navigation/navigationTypes";
+import theme from "../theme";
+import { ParsedBarcodeEvent } from "../types/barcodeEvent";
+import { DetectedBarcodeTypes } from "../utils/detectBarcodeType";
+import { parseBarcode } from "../utils/parseBarcode";
 
 export const ScanResultScreen: React.FC = () => {
   const route = useRoute<ScanResultScreenRouteProp>();
-  const scanResult = route?.params?.barcodeEvent;
+  const id = route?.params?.id;
+  const [isLoading, setisLoading] = React.useState(true);
+  const [barcodeEventWithTypes, setbarcodeEventWithTypes] = React.useState<
+    ParsedBarcodeEvent
+  >();
+
+  React.useEffect(() => {
+    AsyncStorage.getItem(id)
+      .then((data) => data && JSON.parse(data))
+      .then((parsedData) => setbarcodeEventWithTypes(parsedData))
+      .then(() => setisLoading(false));
+  }, []);
 
   return (
     <View style={styles.container}>
-      <ScanResult scanResult={scanResult} />
+      {isLoading && (
+        <ActivityIndicator size="large" color={theme.primaryDark} />
+      )}
+      {barcodeEventWithTypes && (
+        <HistoryItem parsedItem={barcodeEventWithTypes} />
+      )}
     </View>
   );
 };
