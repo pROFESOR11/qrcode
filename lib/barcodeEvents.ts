dfabbbe0ab700430a318652fb8b1351c47b8e943
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-16 19:07:30 +0300

    save processed barcode content on scan

--- lib/barcodeEvents.ts
+++ lib/barcodeEvents.ts
@@ -1,14 +1,25 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { BarCodeEvent } from "expo-barcode-scanner";
+import { DetectedBarcodeTypes } from "../utils/detectBarcodeType";
 import { uuidv4 } from "../utils/uuid";
 
-export function saveBarcodeEvent(barcodeEvent: BarCodeEvent) {
-  const { data, type } = barcodeEvent;
+export function saveBarcodeEvent(
+  barcodeEvent: BarCodeEvent & { details: DetectedBarcodeTypes }
+) {
+  const { data, type, details } = barcodeEvent;
   try {
+    const uuid = uuidv4();
     AsyncStorage.setItem(
-      uuidv4(),
-      JSON.stringify({ data, type, isFavourite: false, date: new Date() })
+      uuid,
+      JSON.stringify({
+        data,
+        type,
+        isFavourite: false,
+        date: new Date(),
+        details,
+      })
     );
+    return uuid;
   } catch (error) {
     console.log("error", error);
   }
