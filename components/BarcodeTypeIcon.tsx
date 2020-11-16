4516c6409f680b67c3517cf928890091467220de
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-11-16 20:48:03 +0300

    render same icon for all types in history item

--- components/BarcodeTypeIcon.tsx
+++ components/BarcodeTypeIcon.tsx
@@ -2,7 +2,7 @@ import { Icon } from "react-native-elements";
 import theme from "../theme";
 import React from "react";
 
-export const BarcodeTypeIcon = ({ item }: { item: any }) => {
+export const BarcodeTypeIcon = () => {
   return (
     <Icon
       type="material-community"
