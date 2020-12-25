d96cceb98e992c5beb2b9ea91a70de9f3ef8e9c3
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-12-25 21:39:06 +0300

    track screen views

--- App.tsx
+++ App.tsx
@@ -1,15 +1,42 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from "@react-navigation/native";
+import * as Analytics from 'expo-firebase-analytics';
+import { Container } from "native-base";
 import { StatusBar } from "react-native";
 import { RootNavigator } from "./navigation/root/RootNavigator";
 
 export default function App() {
+  const routeNameRef = React.useRef<string>();
+  const navigationRef = React.useRef<NavigationContainerRef>(null);
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <RootNavigator />
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() =>
+        routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name
+      }
+      onStateChange={() => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name
+
+        if (previousRouteName !== currentRouteName) {
+          // The line below uses the expo-firebase-analytics tracker
+          // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
+          // Change this line to use another Mobile analytics SDK
+          Analytics.setCurrentScreen(currentRouteName);
+        }
+
+        // Save the current route name for later comparision
+        routeNameRef.current = currentRouteName;
+      }}
+    >
+      <Container>
+        <StatusBar barStyle="light-content" />
+        <RootNavigator />
+      </Container>
     </NavigationContainer>
   );
 }
